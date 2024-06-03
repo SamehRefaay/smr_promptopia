edbae6ceea9c3f6d21e0af1a33ea72d790319e49
SamehRefaay <samehrefaay89@gmail.com>
2024-06-03 21:44:13 +0300

    fixing typo

--- src/components/ProfileInfo.tsx
+++ src/components/ProfileInfo.tsx
@@ -24,44 +24,47 @@ const ProfileInfo = ({ userId }: { userId: string }) => {
 		fetchUserData();
 	}, []);
 	return (
-		<div className="grid gap-5 grid-cols-1 md:grid-cols-3">
-			<div className="w-full p-5 shadow-md bg-white grid place-content-center">
-				<div className="text-center">
-					<Image
-						className="rounded-full mx-auto"
-						src={user?.image}
-						alt="profile image"
-						width={200}
-						height={200}
-					/>
-					<h1 className="text-xl text-gray-900 font-bold font-satoshi">
-						{user?.name}
-					</h1>
-					<p className="text-sm text-gray-400 font-inter">{`@${user?.username}`}</p>
-					<p className="text-sm text-gray-400 font-inter">{user?.email}</p>
-					<div className="flex gap-1 mt-4">
-						<button className="w-32 h-10 rounded-lg flex-center border-[1px] border-black hover:border-none hover:bg-black hover:text-white">
-							Follow
-						</button>
-						<button className="w-32 h-10 rounded-lg flex-center border-[1px] border-black hover:border-none hover:bg-black hover:text-white">
-							Message
-						</button>
+		user && (
+			<div className="grid gap-5 grid-cols-1 md:grid-cols-3">
+				<div className="w-full p-5 shadow-md bg-white grid place-content-center">
+					<div className="text-center">
+						<Image
+							className="rounded-full mx-auto"
+							src={user?.image}
+							alt="profile image"
+							width={200}
+							height={200}
+						/>
+						<h1 className="text-xl text-gray-900 font-bold font-satoshi">
+							{user?.name}
+						</h1>
+						<p className="text-sm text-gray-400 font-inter">{`@${user?.username}`}</p>
+						<p className="text-sm text-gray-400 font-inter">{user?.email}</p>
+						<div className="flex gap-1 mt-4">
+							<button className="w-32 h-10 rounded-lg flex-center border-[1px] border-black hover:border-none hover:bg-black hover:text-white">
+								Follow
+							</button>
+							<button className="w-32 h-10 rounded-lg flex-center border-[1px] border-black hover:border-none hover:bg-black hover:text-white">
+								Message
+							</button>
+						</div>
 					</div>
 				</div>
+				<div className="md:col-span-2 p-5">
+					<h2 className="text-xl font-satoshi font-semibold">About me</h2>
+					<p className="mt-5 text-gray-400 font-inter">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+						ut, natus similique ex non facere quibusdam eveniet suscipit rerum
+						architecto voluptatum explicabo maxime exercitationem omnis quasi
+						sunt laboriosam possimus! Eaque. Lorem ipsum dolor sit amet
+						consectetur adipisicing elit. Dolore, earum quaerat veniam debitis
+						asperiores saepe, voluptatem fuga inventore omnis excepturi
+						incidunt! Sapiente eligendi officiis error dolorum velit porro
+						ratione amet?
+					</p>
+				</div>
 			</div>
-			<div className="md:col-span-2 p-5">
-				<h2 className="text-xl font-satoshi font-semibold">About me</h2>
-				<p className="mt-5 text-gray-400 font-inter">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ut,
-					natus similique ex non facere quibusdam eveniet suscipit rerum
-					architecto voluptatum explicabo maxime exercitationem omnis quasi sunt
-					laboriosam possimus! Eaque. Lorem ipsum dolor sit amet consectetur
-					adipisicing elit. Dolore, earum quaerat veniam debitis asperiores
-					saepe, voluptatem fuga inventore omnis excepturi incidunt! Sapiente
-					eligendi officiis error dolorum velit porro ratione amet?
-				</p>
-			</div>
-		</div>
+		)
 	);
 };
 

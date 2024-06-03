020963c4e58d0f0fec9a2ae821bb955ca8bc826f
SamehRefaay <samehrefaay89@gmail.com>
2024-06-03 21:12:07 +0300

    fix profile isssu

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,21 +1,21 @@
 import ProfilePrompts from '@/components/ProfilePrompts';
 import ProfileInfo from '@/components/ProfileInfo';
 
-const fetchUserPrompts = async (userId: string) => {
-	const response = await fetch(
-		`${process.env.BASE_URL}/api/prompt/users/${userId}/posts`,
-		{ cache: 'no-store' }
-	);
-
-	if (!response.ok) {
-		throw new Error('Something went wrong!');
-	}
-
-	return response.json();
-};
-
 const ProfilePage = async ({ params }: { params: { id: string } }) => {
 	const { id } = params;
+
+	const fetchUserPrompts = async (userId: string) => {
+		const response = await fetch(`/api/prompt/users/${userId}/posts`, {
+			cache: 'no-store',
+		});
+
+		if (!response.ok) {
+			throw new Error('Something went wrong!');
+		}
+
+		return response.json();
+	};
+
 	const posts = await fetchUserPrompts(id);
 
 	return (
